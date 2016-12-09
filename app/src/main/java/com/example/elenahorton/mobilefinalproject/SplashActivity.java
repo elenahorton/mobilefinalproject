36978a358519c0d949d3ee8c9d8ad1807c94aca7
Elena Horton <elena.n.horton.18@dartmouth.edu>
2016-12-09 16:57:39 +0100

    all files hopefully

---
+++ app/src/main/java/com/example/elenahorton/mobilefinalproject/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.elenahorton.mobilefinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+        private final static int HI = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
